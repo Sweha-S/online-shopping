acda4e3f3cf1e3bddf5788f366d69f38b4896f53
Sweha-S <34002421+Sweha-S@users.noreply.github.com>
2017-12-10 14:50:05 +0530

    Add files via upload
    backend project

---
+++ shoppingbackend/src/main/java/Ecom/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,16 @@
+package Ecom.shoppingbackend.dao;
+
+import java.util.List;
+
+import Ecom.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	Category get(int id);
+	List<Category> list();
+	
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+}
---
+++ shoppingbackend/src/main/java/Ecom/shoppingbackend/dao/ProductDAO.java
@@ -0,0 +1,20 @@
+package Ecom.shoppingbackend.dao;
+
+import java.util.List;
+
+import Ecom.shoppingbackend.dto.Product;
+
+public interface ProductDAO {
+
+	Product get(int productId);
+	List<Product> list();
+	
+	boolean add(Product product);
+	boolean update(Product product);
+	boolean delete(Product product);
+	
+	
+	List<Product> listActiveProducts();
+	List<Product> listActiveProductsByCategory(int categoryId);
+	List<Product> getLatestActiveProducts(int count);
+}
